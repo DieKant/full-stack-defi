75ef89d271d88bdf615c391787dcd55405fb5f56
DieKant <olmastroni.fedro2000@gmail.com>
2021-12-03 00:12:07 +0100

    FIX: removed unused variable FORKED_LOCAL_ENVIRONMENTS from get_account function

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -24,7 +24,6 @@ def get_account(index=None, id=None):
         return accounts.load(id)
     if (
         network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
